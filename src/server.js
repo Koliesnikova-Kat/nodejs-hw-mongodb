bcfe975b6d8e01f1f44aaa30bf3456541953d1f8
Kateryna <koliesnikova.kateryna@gmail.com>
2025-04-13 00:57:45 +0300

    fixed

--- src/server.js
+++ src/server.js
@@ -30,18 +30,22 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-    if (!contact) {
-      res.status(400).json({ message: 'Contact not found' });
-      return;
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+      if (!contact) {
+        res.status(400).json({ message: 'Contact not found' });
+        return;
+      }
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      next(error);
     }
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contact with id {contactId}!',
-      data: contact,
-    });
   });
 
   app.use((req, res) => {
