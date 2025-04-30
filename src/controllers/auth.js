513e6e39a4fed19f4f6511a6c5b0f37c5520a226
Kateryna <koliesnikova.kateryna@gmail.com>
2025-04-30 19:43:40 +0300

    google fixed

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -101,6 +101,7 @@ export const resetPasswordController = async (req, res) => {
 
 export const getGoogleOAuthUrlController = async (req, res) => {
   const url = generateAuthUrl();
+  
   res.json({
     status: 200,
     message: 'Successfully get Google OAuth url!',
