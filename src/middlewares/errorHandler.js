2b40e94e4dcad988319be2fda760272b14ab1c49
Kateryna <152926745+Koliesnikova-Kat@users.noreply.github.com>
2025-05-02 00:45:48 +0300

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,25 +2,17 @@ import { HttpError } from 'http-errors';
 
 // eslint-disable-next-line no-unused-vars
 export const errorHandler = (err, req, res, next) => {
-  if (res.headersSent) {
-    return next(err);
-  }
-
   if (err instanceof HttpError) {
-    return res.status(err.status).json({
+    res.status(err.status).json({
       status: err.status,
       message: err.name,
-      data: {
-        message: err.message,
-      },
+      data: err,
     });
   }
 
-  return res.status(500).json({
+  res.json({
     status: 500,
-    message: 'InternalServerError',
-    data: {
-      message: 'Something went wrong',
-    },
+    message: 'Something went wrong',
+    data: '',
   });
 };
