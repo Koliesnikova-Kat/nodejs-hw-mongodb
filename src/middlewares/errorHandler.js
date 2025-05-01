8e5863f60ae7a202d060c967f7d64292415cf484
Kateryna <152926745+Koliesnikova-Kat@users.noreply.github.com>
2025-05-01 14:21:33 +0300

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -7,7 +7,7 @@ export const errorHandler = (err, req, res, next) => {
   }
 
   if (err instanceof HttpError) {
-    res.status(err.status).json({
+    return res.status(err.status).json({
       status: err.status,
       message: err.name,
       data: {
